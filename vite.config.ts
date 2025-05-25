0a8a9bea5d810061841c9fa1414356d43748a36d
Mohamed Mushtaq <mohamedmush012@gmail.com>
2025-05-25 15:52:51 +0530

    Update content

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/praja-vote/', // Correct base for GitHub Pages
   plugins: [react()],
-  base: '/praja-vote/',
+  server: {
+    port: 3005,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  server: {
-    port: 3005, // Change to any desired port
-  },
+  }
 });
